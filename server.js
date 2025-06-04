4a717bcd5bf8de0712eeab069b20d961cecb4070
thedavids <31598577+thedavids@users.noreply.github.com>
2025-06-04 06:33:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('Socket.IO server running on port 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Socket.IO server running on port ${PORT}`);
 });
