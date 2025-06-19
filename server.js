abc7d5c721ab884eeb2e7500bee98aca8e56d84e
thedavids <31598577+thedavids@users.noreply.github.com>
2025-06-19 16:43:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ const maps = {
   bigCity: {
   "name": "Big City",
   "objects": [
-    {"position": {"x": 0,"y": 65,"z": 0},"size": [332,140,302],"rotation": {"x": 0,"y": 0,"z": 0},"scale": {"x": 1.990831431526063,"y": 1.988958187444187,"z": 1.999921682822281},"type":"box","file":"https://www.dailysummary.io/models/FullCity.glb","model":"Sketchfab_Scene","offset": {"x": -6.283851623535156,"y": 34.458319501665684,"z": 6.247860158213669}}
+    {"position": {"x": 0,"y": 0,"z": 0},"size": [332,140,302],"rotation": {"x": 0,"y": 0,"z": 0},"scale": {"x": 1.990831431526063,"y": 1.988958187444187,"z": 1.999921682822281},"type":"box","file":"https://www.dailysummary.io/models/FullCity.glb","model":"Sketchfab_Scene","offset": {"x": -6.283851623535156,"y": 34.458319501665684,"z": 6.247860158213669}}
   ],
   "healthPacks": []
 }
